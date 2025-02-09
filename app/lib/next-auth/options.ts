2416b06ebfeda3821464f8442e063329c0d4d99f
shuya <sz.shuya1231@gmail.com>
2025-02-10 01:04:30 +0900

    create prisma instance

---
+++ app/lib/next-auth/options.ts
@@ -0,0 +1,28 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import prisma from "../prisma";
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+};
